d1ea6e1dd9161b2fafb58c9a6c1ed07890c32cfd
AndonMitev <mitevandon94@gmail.com>
2017-11-27 00:16:25 +0200

    PHP Blog with MySQL and Symfony

---
+++ PHP-MVC-and-Symfony-Overview-Lab/src/SoftUniBlogBundle/Form/ArticleType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SoftUniBlogBundle\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add("content", TextType::class)
+            ->add("title", TextType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SoftUniBlogBundle\Entity\Article',
+        ));
+    }
+}
